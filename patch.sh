c47e8ad8ffc1ed518db40aba173a6d4c5d1d4d5f
Willie Shen <cg10250207@gmail.com>
2026-09-19 00:00:29 +0800

    2026-09-18T16:00:29+00:00

--- patch.sh
+++ patch.sh
@@ -2,5 +2,5 @@
 
 # shellcheck disable=2034
 git clone --depth=1 https://github.com/termux/termux-packages.git
-PATCH="sed -Ei 's#^([[:space:]]*)(\|[[:space:]]*VIRTUAL_DISPLAY_FLAG_ALWAYS_UNLOCKED)#\1// \2#' server/src/main/java/com/genymobile/scrcpy/video/NewDisplayCapture.java" perl -0pi -E 's/(termux_step_post_get_source\(\) {\n)([ \t]*)/$1$2$ENV{PATCH}\n$2/' termux-packages/x11-packages/scrcpy/build.sh
+PATCH="sed -Ei 's#^([[:space:]]*)(\|[[:space:]]*VIRTUAL_DISPLAY_FLAG_ALWAYS_UNLOCKED)#\1// \2#' server/src/main/java/com/genymobile/scrcpy/video/NewDisplayCapture.java" CAT="server/src/main/java/com/genymobile/scrcpy/video/NewDisplayCapture.java" perl -0pi -E 's/(termux_step_post_get_source\(\) {\n)([ \t]*)/$1$2$ENV{PATCH}\n$2$ENV{CAT}\n$2/' termux-packages/x11-packages/scrcpy/build.sh
 cat termux-packages/x11-packages/scrcpy/build.sh
