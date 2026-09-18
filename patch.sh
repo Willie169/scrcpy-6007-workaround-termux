e891025d45f127b04e2595009fb8bc5059f04b38
Willie Shen <cg10250207@gmail.com>
2026-09-19 00:53:33 +0800

    2026-09-18T16:53:33+00:00

--- patch.sh
+++ patch.sh
@@ -2,5 +2,5 @@
 
 # shellcheck disable=2034
 git clone --depth=1 https://github.com/termux/termux-packages.git
-OLD='local url="https://github.com/Genymobile/scrcpy/releases/download/v${TERMUX_PKG_VERSION}/${scrcpy_server}"' NEW='local url="https://github.com/Willie169/scrcpy-6007-workaround/releases/download/v${TERMUX_PKG_VERSION}/${scrcpy_server}"' perl -0pi -E 's/$ENV{OLD}/$ENV{NEW}/' termux-packages/x11-packages/scrcpy/build.sh
+OLD='local url="https://github.com/Genymobile/scrcpy/releases/download/v${TERMUX_PKG_VERSION}/${scrcpy_server}"' NEW='local url="https://github.com/Willie169/scrcpy-6007-workaround/releases/download/v${TERMUX_PKG_VERSION}/${scrcpy_server}"' perl -0pi -E 's/\Q$ENV{OLD}\E/$ENV{NEW}/' termux-packages/x11-packages/scrcpy/build.sh
 cat termux-packages/x11-packages/scrcpy/build.sh
